1e6c42f2e8dc739bec56e2cb82095f1470967194
Jarrod Bogard <jarrodbogard@gmail.com>
2024-04-17 08:19:39 -0400

    Add "quit" key
    Add quit application to the escape key

---
+++ Assets/Environment/Land Tiles/QuitApplication.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class QuitApplication : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Exit Game");
+            Application.Quit();
+        }
+    }
+}
